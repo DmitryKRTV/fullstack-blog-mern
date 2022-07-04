43e9a492a65ed3f7ac66fd59e4030db7784a821c
DmitryKRTV <dmitrykorotaev.job@gmail.com>
2022-07-04 12:23:08 +0300

    Commit 4. Added authorization

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import {validationResult} from "express-validator";
 import {registerValidation} from "./validations/auth.js"
 
 import UserModule from "./models/User.js"
+import {mongoDBURL} from "./secretInfo/mongoDB.js";
 
-const mongoBDName = "blog" // укащываем то, как мы хотим назвать нашу базу данных, без неё мы просто подключаемся к серверу
+
 
 mongoose
-    .connect(`mongodb+srv://admin:1OQgE4k3ze9aypEz@cluster0.ytogzao.mongodb.net/${mongoBDName}?retryWrites=true&w=majority`)
+    .connect(mongoDBURL)
     .then(i => console.log("DB ok"))
     .catch(err => console.log("Db error", err))
 
@@ -20,30 +21,85 @@ const app = express();
 app.use(express.json());
 
 
-app.post("/auth/register",registerValidation, async (req, res)=>{
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
+app.post("/auth/register", registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
+
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt)
+
+        const doc = new UserModule({
+            email: req.body.email, passwordHash: hash, fullName: req.body.fullName, avatarUrl: req.body.avatarUrl,
+        });
+
+        const user = await doc.save();
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, "secret123", {
+            expiresIn: "30d"
+        })
+
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json({
+            ...userData, token,
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Не удалось зарегестрироваться",
+        })
     }
 
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    const doc = new UserModule({
-        email: req.body.email,
-        passwordHash,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-    });
-
-    const user = await doc.save();
-
-    res.json(user)
 })
 
-app.listen(4444, (err)=> {
-    if(err) {
+app.post("/auth/login", async (req, res) => {
+    try {
+        const user = await UserModule.findOne({ email: req.body.email});
+
+        if(!user) {
+            return res.status(404).json({
+                message: "Неверный логин или пароль",
+            })
+        }
+
+        const isValidPassword = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if(!user) {
+            return res.status(404).json({
+                message: "Неверный логин или пароль",
+            })
+        }
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, "secret123", {
+            expiresIn: "30d"
+        })
+
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json({
+            ...userData, token,
+        })
+
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Не удалось авторизоваться",
+        })
+    }
+})
+
+app.listen(4444, (err) => {
+    if (err) {
         return console.log(err)
     }
     console.log("Server OK")
---
+++ server/secretInfo/mongoDB.js
@@ -0,0 +1,3 @@
+const mongoBDName = "blog" // укащываем то, как мы хотим назвать нашу базу данных, без неё мы просто подключаемся к серверу
+
+export const mongoDBURL = `mongodb+srv://admin:1OQgE4k3ze9aypEz@cluster0.ytogzao.mongodb.net/${mongoBDName}?retryWrites=true&w=majority`
