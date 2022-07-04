82d83380fa8ff54ff247c5fab3417cab2f8c655e
DmitryKRTV <dmitrykorotaev.job@gmail.com>
2022-07-04 12:30:15 +0300

    Commit 4.6. Fixed bugs in authorization

--- server/index.js
+++ server/index.js
@@ -64,7 +64,7 @@ app.post("/auth/login", async (req, res) => {
         const user = await UserModule.findOne({ email: req.body.email});
 
         if(!user) {
-            return req.status(404).json({
+            return res.status(404).json({
                 message: "Неверный логин или пароль",
             })
         }
@@ -72,7 +72,7 @@ app.post("/auth/login", async (req, res) => {
         const isValidPassword = await bcrypt.compare(req.body.password, user._doc.passwordHash);
 
         if(!isValidPassword) {
-            return req.status(404).json({
+            return res.status(404).json({
                 message: "Неверный логин или пароль",
             })
         }
